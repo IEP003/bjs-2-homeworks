913a09517c3c3f1323f74a9aa84df8c62c720619
Igor <igor@i-petchenko.ru>
2022-11-30 19:56:30 +0300

    Исправленное 1 задание и выполненое 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,10 +3,10 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d = b**2 - 4 * a * c;
   if (d > 0) {
-    arr.push ((-b + Math.sqrt(d) ) / (2 * a));
-    arr.push ((-b - Math.sqrt(d) ) / (2 * a));
+    arr.push((-b + Math.sqrt(d) ) / (2 * a));
+    arr.push((-b - Math.sqrt(d) ) / (2 * a));
   } else if (d === 0) {
-    arr.push (-b / (2 * a));
+    arr.push(-b / (2 * a));
   }
   return arr;
 }
@@ -15,16 +15,12 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let totalAmont;
   let monthlyPayment;
-  let monthPercent = percent / 100 / countMonths; 
+  let monthPercent = percent / 100 / 12; 
   let loanBody = amount - contribution;
 
-  if (isNaN(percent)){
-    alert(`«Параметр <percent> содержит неправильное значение ».${percent}`);
-  } else if (isNaN(contribution) === false){
-    alert(`«Параметр <contribution> содержит неправильное значение ».${contribution}`);
-  } else if (isNaN(amount) === false){
-    alert(`«Параметр <amount> содержит неправильное значение ».${amount}`);
-  };
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) == true){
+    return false;
+  }
 
   monthlyPayment = loanBody * (monthPercent + (monthPercent / (((1 + monthPercent)**countMonths) - 1)));
   totalAmont = parseFloat((monthlyPayment * countMonths).toFixed(2));
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,81 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
 
+  for (let i = 0; i < arr.length; i++){
+    if (max < arr[i]){
+      max = arr[i];
+    } else if (min > arr[i]){
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = parseFloat((sum / arr.length).toFixed(2))
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length > 0){
+    const sumWithInitial = arr.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    return sumWithInitial;
+  } else {
+    return 0;
+  }
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length > 0) {
+    return Math.max(...arr) - Math.min(...arr);
+  } else {
+    return 0;
+  }
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  
+  if (arr.length > 0){
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] % 2 == 0){
+        sumEvenElement += arr[i];
+      } else {
+        sumOddElement += arr[i];
+      }
+    }
+    return sumEvenElement - sumOddElement;
+  } else {
+    return 0;
+  }
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let countEvenElement = 0; 
+  if (arr.length > 0){
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] % 2 == 0){
+        sumEvenElement += arr[i];
+        countEvenElement ++;
+      }
+    }
+    return sumEvenElement / countEvenElement;
+  } else {
+    return 0;
+  }   
 }
 
 function makeWork (arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
+  const result = [];
 
+  for (let i = 0; i < arrOfArr.length; i++){
+    result.push(func(...arrOfArr[i]));
+  } if (Math.max(...result) > maxWorkerResult) {
+    maxWorkerResult = Math.max(...result)
+  }
+  return maxWorkerResult;
 }
