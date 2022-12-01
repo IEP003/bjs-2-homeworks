2b510eebbec80b0ee3ea76f785a21a6b3629c145
Igor <igor@i-petchenko.ru>
2022-12-01 18:32:55 +0300

    Выполненное задание №3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length){
+        return arr1.every((number, index) => number === arr2[index]);
+    } else {
+        return false;
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+   return users
+     .filter((user) => user.gender === gender)
+     .map((user) => user.age)
+     .reduce((acc, item, index, array) => acc + item / array.length, 0)
 }
