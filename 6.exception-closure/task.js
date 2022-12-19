da1053ea59ea179a5fd9813576261a1b20b90da0
Igor <igor@i-petchenko.ru>
2022-12-19 15:40:37 +0300

    6.exception-closure Complete

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,13 +1,48 @@
 ﻿function parseCount(number){
-    if(Number.parseFloat(number) == NaN){
-        throw new Error('невалидное значение')
+    if(isNaN(Number.parseFloat(number))){
+        throw new Error("Невалидное значение")
     } return Number.parseFloat(number)
 }
 
-function validateCount(number){
+function validateCount(difrentNumber){
     try{
-        parseCount(number)
+        return parseCount(difrentNumber)
     } catch (error){
-        
+        return error
+    } 
+}
+
+class Triangle {
+    constructor (a,b,c){
+        if (a + b < c || b + c < a || a + c < b) {
+            throw new Error('Треугольник с такими сторонами не существует')
+        }
+        this.a = a,
+        this.b = b,
+        this.c = c
+    }
+
+    get perimeter(){
+        return this.a + this.b + this.c;
+    }
+
+    get area(){
+        const halfP = this.perimeter / 2 
+        return  Number(Math.sqrt(halfP*(halfP - this.a)*(halfP - this.b)*(halfP - this.c)).toFixed(3))
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c)
+    } catch (error) {
+        return {
+            get perimeter(){
+                return "Ошибка! Треугольник не существует"
+            },
+            get area(){
+                return "Ошибка! Треугольник не существует"
+            }
+        }
     }
 }
