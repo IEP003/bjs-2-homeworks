cd27170cef04d06767c14f8beec20398d4062ad1
Igor <igor@i-petchenko.ru>
2022-12-29 14:38:37 +0300

    task 8.decorators complete

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,44 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+  let cache = [];
   
+  return (...args) => {
+    const hash = md5(args);
+    let objectInCache = cache.find((object) => object.hash === hash)
+    if(objectInCache){
+        return `Из кэша: ${objectInCache.result}`;
+    }
+
+    const result = func(...args);
+    cache.push({hash, result});
+    if(cache.length > 5){
+        cache.shift();
+    }
+    return `Вычисляем: ${result}`;
+  }
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  let isCooldown = true;
+  decoratedFunction.allCount = 0;
+  decoratedFunction.count = 0;
+  function decoratedFunction(...args){
+    decoratedFunction.allCount ++;
+    if(timeoutId){
+        clearTimeout(timeoutId);
+    }
+    if(isCooldown){
+      decoratedFunction.count ++;
+      func(...args);
+      isCooldown = false;
+    }
+    timeoutId = setTimeout(() => {
+      decoratedFunction.count ++;
+      timeoutId = null;
+      func(...args);
+    }, delay);
+  }
+  return decoratedFunction;
 }
